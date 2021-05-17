10e3c8981c678251d24c694f91feda84c3583b78
TopGitSan <c.toparcean@gmail.com>
2021-05-17 16:38:06 +0200

    add log

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/' ,(req,res)=>{
-  res.send('<h2>Hi there</h2>');
+  console.log("request to root");
+  res.send('<h2>Hi there!!!!</h2>');
 })
 
 const port = process.env.PORT || 3000;
