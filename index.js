5fd259db8911307627df2508a585ea30b8ee0d8d
TopGitSan <c.toparcean@gmail.com>
2021-05-31 12:07:12 +0200

    retry connecting if the mongo instance isn't up

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/c
 
 const app = express();
 
-const mongoURL =`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("succesfully connected to mongoDB"))
-  .catch((e) => console.log(e));
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("succesfully connected to mongoDB"))
+    .catch((e) => {
+      console.log(e);
+      // TO DO only if service error
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   console.log("request to root");
