61a88d9c0fe17a4412b2a97910e229042f608f97
TopGitSan <c.toparcean@gmail.com>
2021-05-31 11:16:36 +0200

    add mongoose to project and connect to db

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get('/' ,(req,res)=>{
+mongoose
+  .connect("mongodb://topdevop:topdevoppass@172.25.0.2:27017/?authSource=admin", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("succesfully connected to mongoDB"))
+  .catch((e) => console.log(e));
+
+app.get("/", (req, res) => {
   console.log("request to root");
-  res.send('<h2>Hi there</h2>');
-})
+  res.send("<h2>Hi there</h2>");
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
