50d125cc25c0eaec6e865e3ab16c4c6b78ddd557
TopGitSan <c.toparcean@gmail.com>
2021-05-31 11:49:59 +0200

    use env var, config dev env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express();
 
+const mongoURL =`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
 mongoose
-  .connect("mongodb://topdevop:topdevoppass@mongo:27017/?authSource=admin", {
+  .connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
